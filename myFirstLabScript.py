9e59bb0393c2b5a951c71c65b49161c49d2e8a77
Samet SARIKAYA <13sametsarikaya@gmail.com>
2025-02-20 15:02:18 +0300

    Create myFirstLabScript.py

---
+++ myFirstLabScript.py
@@ -0,0 +1,28 @@
+#1
+name = input('What is your name? \n')
+print('Hello', name, '\n')
+id = int(input('What is your Student ID? \n'))
+print('Your ID is', id, '.')
+#2
+var1 = int(input('Enter a variable= \n'))
+var2 = int(input('Enter a variable= \n'))
+sum = var1 + var2
+diff = abs(var1 - var2)
+prod = var1 * var2
+print("Variables:", var1, " - ", var2)
+print('Sum = ', sum, "\n")
+print('Diff = ', diff, "\n")
+print('Prod = ', prod, "\n")
+#3
+name = input('What is your name? \n')
+lab = float(input('What is your lab grade? \n'))
+midterm = float(input('Enter a midterm grade= \n'))
+final = float(input('What is your final grade? \n'))
+score = lab*(25/100) + midterm*(35/100) + final*(40/100)
+print("Name: ", name)
+print("Lab: ", lab)
+print("Midterm: ", midterm)
+print("Final: ", final)
+print("Last Score: ", score)
+#4
+print("*\n**\n***\n**\n*\n")
